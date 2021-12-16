5290e0082415b44e945ea5242129bafc96007bdf
Amit Joseph <me@amitjoseph.com>
2021-12-16 11:14:08 +0530

    refactor completer

--- completer/bash.sh
+++ completer/bash.sh
@@ -5,8 +5,8 @@ _awsuser()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
     # echo "=> $prev --- $cur --> ${COMP_WORDS[@]}"
-    opts="create delete reset-console-login search"
-    opts_flags="--console-login --console-login --force --verbose"
+    opts="create delete reset-console-login search describe"
+    opts_flags="--login --access-keys --mfa --force --verbose --groups"
     # opts_users="`awsuser_completor users`"
 
     if [[ ${prev} == awsuser ]] ; then
